b824f732fbc455e1fdb0e58584cd0ef31403e634
aref-elyassi <aref_elyassi1992@yahoo.com>
2025-04-17 17:29:48 +0330

    add some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
-  
+  const [person, setPerson] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('http://localhost:4000/data')
+        const data = await res.json();
+        setPerson(data)
+        console.log(data);
+      } catch (err) {
+        console.error('Error fetching data:', err)
+        setError(err.message)
+      }
+    }
+    
+    fetchData(); // Actually calling the function
+  }, [])
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
   return (
     <>
-     <h1>Hello</h1>
+     
+     {
+      person.map(p=>(
+        <h1>{p.name}</h1>
+      ))
+     }
     </>
   )
 }
